93aca343b48a6ba7ac4410027ebeb91b217ddd4d
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-04-30 14:07:48 +0700

    Mengambil data dari database

--- app/src/main/java/org/d3if0008/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if0008/hitungbmi/ui/hitung/HitungFragment.kt
@@ -64,11 +64,6 @@ class HitungFragment : Fragment() {
             binding.kategoriTextView.text = getString(R.string.kategori_x, getKategori(it.kategori))
             binding.buttonGroup.visibility = View.VISIBLE
         })
-
-        viewModel.data.observe(viewLifecycleOwner, {
-            if (it == null) return@observe
-            Log.d("HitungFragment", "Data tersimpan. ID = ${it.id}")
-        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
